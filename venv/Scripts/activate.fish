8187bfb2eaeb6fdf01f8a9545518233e5c387a88
Choudhary Hussain <choudharyhussain11@gmail.com>
2025-09-25 17:49:56 +0530

    Setup SmartStudentAI on new laptop

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Administrator\SmartStudentAI\venv'
+set -gx VIRTUAL_ENV 'C:\Users\Mohammed Hussain\OneDrive\Desktop\SmartStudentAI\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
